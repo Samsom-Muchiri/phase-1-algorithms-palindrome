68b954038a0ab8e1622df8bc000ffdb65b1ff47e
Sam .M <you@example.com>
2023-06-27 13:26:08 +0300

    complete

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const compare = word.split('').reverse('').join('')
+    return word === compare
 }
 
 /* 
   Add your pseudocode here
+  split the string to an array
+  reverse the array 
+  join the array 
+  compare the joined array with the main string
 */
 
 /*
   Add written explanation of your solution here
+
+  if raversed string is equal to the original array return true else fale
 */
 
 // You can run `node index.js` to view these console logs
@@ -16,10 +24,12 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log(isPalindrome("abba"));
+  console.log(isPalindrome("a"));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+  console.log(isPalindrome("ab"));
 }
 
 module.exports = isPalindrome;
